19f4cb1c88eb37cd27a915e4376832b93c899676
Marie <Marue59@hotmail.fr>
2022-06-09 16:00:14 +0200

    creation d'un champs hidden

---
+++ src/app/components/magie/magie.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-magie',
+  templateUrl: './magie.component.html',
+  styleUrls: ['./magie.component.scss']
+})
+export class MagieComponent implements OnInit {
+
+  hidden: boolean = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggle() {
+    this.hidden = !this.hidden
+  }
+
+}
