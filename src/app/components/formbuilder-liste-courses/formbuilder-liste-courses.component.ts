31f9a689b4e31ed703912f950b98a50a43fd2f98
Marie <Marue59@hotmail.fr>
2022-06-14 10:08:59 +0200

    utilisation de form

--- src/app/components/formbuilder-liste-courses/formbuilder-liste-courses.component.ts
+++ src/app/components/formbuilder-liste-courses/formbuilder-liste-courses.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { retry } from 'rxjs';
+
 @Component({
   selector: 'app-formbuilder-liste-courses',
   templateUrl: './formbuilder-liste-courses.component.html',
